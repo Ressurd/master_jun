14042f90d75202a76fccc333c15d80b421fae152
Ressurd <ressurd@gmail.com>
2022-06-19 02:02:34 +0900

    과연
    될까?

---
+++ MJ/src/main/java/com/masterJun/MJ/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.masterJun.MJ;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MjApplication.class);
+	}
+
+}
---
+++ MJ/src/test/java/com/masterJun/MJ/MjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterJun.MJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
