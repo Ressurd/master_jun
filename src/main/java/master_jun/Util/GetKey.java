911ba14351bd99c2a02ddbce7fc9ee8a9434723f
Ressurd <ressurd@gmail.com>
2022-06-19 14:36:02 +0900

    get Key upbit, telegram version 2 GOOD

--- src/main/java/master_jun/Util/GetKey.java
+++ src/main/java/master_jun/Util/GetKey.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.json.JSONObject;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +20,13 @@ public class GetKey {
 	 * @throws IOException 
 	 * @date 가고 싶다
 	 * @since 20220619
+	 * 
 	 */
 	@RequestMapping("/getUpbitKey.do")
-	public ArrayList<String> userGetUpbitKeyInfo() throws IOException {
+	public JSONObject userGetUpbitKeyInfo() throws IOException {
 		BufferedReader bufferdReader = new BufferedReader(
 				new FileReader("D:\\yek\\upbit.key"));
-				
+		JSONObject jo = new JSONObject();
 		String str;
 		ArrayList<String> al = new ArrayList<>();
 		while ((str = bufferdReader.readLine()) != null) {
@@ -32,21 +34,25 @@ public class GetKey {
 		}
 		bufferdReader.close();
 		
-		return al;
+		jo.put("access_key", al.get(0));
+		jo.put("secret_key", al.get(1));
+		
+		return jo;
 	}
 	
 	@RequestMapping("/getTelegramKey.do")
-	public ArrayList<String> userGetTelegramKeyInfo() throws IOException {
+	public JSONObject userGetTelegramKeyInfo() throws IOException {
 		BufferedReader bufferdReader = new BufferedReader(
 				new FileReader("D:\\yek\\telegram.key"));
-				
+		JSONObject jo = new JSONObject();
 		String str;
 		ArrayList<String> al = new ArrayList<>();
 		while ((str = bufferdReader.readLine()) != null) {
 			al.add(str);
 		}
 		bufferdReader.close();
-		
-		return al;
+		jo.put("access_key", al.get(0));
+		jo.put("secret_key", al.get(1));
+		return jo;
 	}
 }
