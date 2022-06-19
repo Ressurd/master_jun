5d6344722e49edf1a531596776ebdc2474efc39d
Ressurd <ressurd@gmail.com>
2022-06-19 19:10:25 +0900

    개 빢 치 네 J S O N  VS S E C U R I T Y

--- src/main/java/master_jun/Service/testServiceImpl.java
+++ src/main/java/master_jun/Service/testServiceImpl.java
@@ -1,8 +1,63 @@
 package master_jun.Service;
 
-import org.springframework.stereotype.Repository;
+import java.io.IOException;
+import java.util.Iterator;
 
-@Repository
+import javax.annotation.Resource;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import master_jun.DAO.testDAO;
+import master_jun.Util.GetKeyUtil;
+
+@Service
 public class testServiceImpl implements testService{
 	
+	@Resource
+	private GetKeyUtil gku = new GetKeyUtil();
+	
+	@Resource
+	private testDAO tDAO = new testDAO();
+	
+	@Override
+	public void insertKey(JSONObject KeyKey) {
+		tDAO.insert(KeyKey);
+	}
+	
+	@Override
+	public JSONObject testJSONReturn() throws IOException{
+		JSONObject json = new JSONObject();
+		json = gku.userGetUpbitKeyInfo();
+		
+		Iterator<String> keys = json.keys();
+		
+		// 혹시 몰라서 foreach 만들어봄 깔까
+		/*
+		 * json.keySet().forEach(keyStr ->{ Object keyvalue = json.get(keyStr); });
+		 */
+		
+		return json;
+	}
+	
+	@Override
+	public JSONObject testJSONReturn2() throws IOException{
+		JSONObject json = new JSONObject();
+		json = gku.userGetTelegramKeyInfo();
+		
+		Iterator<String> keys = json.keys();
+		
+		// 혹시 몰라서 foreach 만들어봄 깔까
+		/*
+		 * json.keySet().forEach(keyStr ->{ Object keyvalue = json.get(keyStr); });
+		 */
+		
+		return json;
+	}
+
+	@Override
+	public JSONObject updateKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
